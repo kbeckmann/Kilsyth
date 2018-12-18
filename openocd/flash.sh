dda6126cce18ce305066907f3cbcc381b23b8d21
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-12-19 00:53:52 +0100

    Fix paths

--- openocd/flash.sh
+++ openocd/flash.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if [ "$TRELLIS" = "" ]; then
+	echo "Set environment variable TRELLIS to your Trellis root"
+	exit
+fi
+
+$TRELLIS/tools/bit_to_svf.py ../gateware/bootloader/bootloader/kilsyth_bootloader.bit ../gateware/bootloader/bootloader/kilsyth_bootloader.svf
+
 openocd -f interface/ftdi/dp_busblaster.cfg -c "transport select jtag; adapter_khz 10000;" -f kilsyth.cfg
+
