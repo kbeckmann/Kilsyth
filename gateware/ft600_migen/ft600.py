1386536e2b5f675ab24d96f0a3e378794500899d
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-12-24 00:59:14 +0100

    Bidirectional transfers seem to work

--- gateware/ft600_migen/ft600.py
+++ gateware/ft600_migen/ft600.py
@@ -2,6 +2,15 @@ from migen import *
 
 
 class FT600Pipe(Module):
+    def build_pin_tristate(self, pin, oe, o, i):
+        self.specials += \
+            Instance("TRELLIS_IO",
+                p_DIR="BIDIR",
+                i_T=oe,
+                i_I=o,
+                o_O=i,
+            )
+
     def __init__(self, clk, leds, ft600):
         self.clock_domains.cd_por = ClockDomain(reset_less=True)
         self.clock_domains.cd_sys = ClockDomain()
@@ -10,6 +19,11 @@ class FT600Pipe(Module):
  
         cd_ft600 = ClockDomain()
         cd_ft600.clk = ft600.clk
+
+        ft_be_i = Signal(2)
+        ft_be_o = Signal(2)
+        ft_be_oe = Signal()
+        self.build_pin_tristate(ft600.be, ft_be_oe, ft_be_o, ft_be_i)
  
         self.clock_domains += cd_ft600
  
@@ -29,6 +43,7 @@ class FT600Pipe(Module):
             # leds[5].eq(ft_rd_n),
             # leds[6].eq(~leds[6]),
             # leds[7].eq(wants_to_write),
+            ft_be_oe.eq(ft_data_oe),
         ]
  
         self.sync += [
@@ -44,13 +59,13 @@ class FT600Pipe(Module):
             ft_oe_n.eq(1),
             ft_wr_n.eq(1),
             ft_rd_n.eq(1),
+            ft_be_oe.eq(0),
             ft_data_oe.eq(0),
-            # If(
-            #     ft600.txe_n == 0 and wants_to_write,
-            #     wants_to_write.eq(0),
-            #     NextState("WRITE-WORD"),
-            # ).Elif(
-            If(ft600.rxf_n == 0,
+            If(
+                ft600.txe_n == 0 and wants_to_write,
+                NextState("WRITE-WORD"),
+            ).Elif(
+                ft600.rxf_n == 0,
                 NextState("READ-WORD"),
             )
         ),
@@ -59,7 +74,8 @@ class FT600Pipe(Module):
             leds[5].eq(1),
             ft_oe_n.eq(0),
             ft_rd_n.eq(0),
-            If(ft600.rxf_n == 1,
+            If(
+                ft600.rxf_n == 1,
                 wants_to_write.eq(1),
                 NextState("WAIT-INPUT")
             )
@@ -67,6 +83,10 @@ class FT600Pipe(Module):
         self.fsm.act(
             "WRITE-WORD",
             leds[3].eq(1),
+            wants_to_write.eq(0),
+            ft_be_oe.eq(1),
+            ft_be_o.eq(0b11),
+            ft_wr_n.eq(0),
             NextState("WAIT-INPUT"),
         )
  
