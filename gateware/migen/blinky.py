d2bd3caf2b17551e7266313549b5423c5bd5c0bf
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-12-24 23:34:58 +0100

    blinky: Add simple test

--- gateware/migen/blinky.py
+++ gateware/migen/blinky.py
@@ -1,14 +1,28 @@
 from migen import *
+import sys
 
 class Blinky(Module):
     def __init__(self, led):
         self.div = Signal(23)
         self.sync += self.div.eq(self.div + 1)
         self.comb += led.eq(self.div[22])
+    def testbench(self):
+        assert (yield self.div[0]) == 0
+        yield
+        assert (yield self.div[0]) == 1
+        yield
+        assert (yield self.div[0]) == 0
+        yield
+        assert (yield self.div[0]) == 1
+        yield
 
 if __name__ == '__main__':
     import kilsyth
     plat = kilsyth.Platform(toolchain='trellis')
     led = plat.request('user_led')
     blinky = Blinky(led)
-    plat.build(blinky, toolchain_path='/usr/local/share/trellis', idcode="0x21111043")
+    if len(sys.argv) == 2 and sys.argv[1] == "test":
+        run_simulation(blinky, blinky.testbench(), vcd_name="out.vcd")
+        print("Passed")
+    else:
+        plat.build(blinky, toolchain_path='/usr/local/share/trellis', idcode="0x21111043")
