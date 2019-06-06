18aa8730676a3cb675ecf70129ac884b9f359a2a
Konrad Beckmann <konrad.beckmann@gmail.com>
2019-06-06 16:38:00 +0200

    Improve ft600 a lot

--- software/kilsyth/applets/ft600_demo/__init__.py
+++ software/kilsyth/applets/ft600_demo/__init__.py
@@ -1,5 +1,5 @@
 from migen import *
-from migen.genlib.fifo import SyncFIFO, AsyncFIFO
+from migen.genlib.fifo import SyncFIFO, AsyncFIFO, AsyncFIFOBuffered
 import os, sys
 
 from .. import KilsythApplet
@@ -14,66 +14,98 @@ class FT600Demo(KilsythApplet, name="ft600_demo"):
 
         led = device.request('user_led')
         ft600_pins = device.request('ft600')
+        clk16 = device.request('clk16')
+
+        device.add_period_constraint("ft600", 1000. / 100)
 
         self.clock_domains.cd_por = ClockDomain()
         self.clock_domains.cd_sys = ClockDomain(reset_less=False)
-        self.cd_sys.clk = ft600_pins.clk
 
-        depth = 2
+        # Posedge
+        self.comb += self.cd_sys.clk.eq(ft600_pins.clk)
+
+        # Negedge
+        # self.comb += self.cd_sys.clk.eq(~ft600_pins.clk)
+
+        self.clock_domains.cd_clk16 = ClockDomain(reset_less=False)
+        self.cd_clk16.clk = clk16
+
+        depth = 1024 * 16
+
         fifo_rx = SyncFIFO(16, depth)
         self.submodules += fifo_rx
 
-        fifo_tx = SyncFIFO(16, depth)
+        fifo_tx = ClockDomainsRenamer({
+            "write": "sys",
+            "read":  "sys",
+        })(AsyncFIFO(16, depth))
         self.submodules += fifo_tx
+
+        # fifo_tx = SyncFIFO(16, depth)
+        # self.submodules += fifo_tx
+
 
         debug = Signal(8)
         self.submodules.ft600 = FT600(ft600_pins, fifo_rx, fifo_tx, debug)
 
+        overflow = Signal()
+        self.comb += [
+            led[0].eq(~fifo_tx.writable),
+            led[1].eq(overflow),
+        ]
 
-        if True:
+        if False:
             # Write as fast as possible
+            # Constantly write to the FIFO,
+            # Increase the counter on each clk
+            # This works 100% perfectly
             counter = Signal(8)
-            overflow = Signal()
             self.comb += [
-                # fifo_tx.din.eq((counter << 8) | counter),
-                # fifo_tx.din.eq(counter),
                 fifo_tx.din.eq(
                     ((counter + 1) << 8) |
                     ((counter    )     )
                 ),
-                fifo_tx.we.eq(1),
             ]
             self.sync += [
                 If (~fifo_tx.writable,
-                    overflow.eq(1)
+                    overflow.eq(1),
+                    fifo_tx.we.eq(0),
+                ).Else(
+                    fifo_tx.we.eq(1),
+                    counter.eq(counter + 2)
                 ),
-                counter.eq(counter + 2)
             ]
         else:
-            # Write every 100th clock cycle
-            # This produces garbled data. Why?
-            # Also it seems to produce a 2x faster data rate than it should...
-            counter = Signal(16)
+            # Write every nth clock cycle
+            counter = Signal(32)
             counter2 = Signal(8)
-            overflow = Signal()
             self.comb += [
                 fifo_tx.din.eq(
                     ((counter2 + 1) << 8) |
                     ((counter2    )     )
                 ),
-                led.eq(overflow),
-                If(counter == 1,
+                If(counter == 0,
                     fifo_tx.we.eq(1),
                 ).Else(
                     fifo_tx.we.eq(0),
                 ),
             ]
 
+
+            # xxd -p -c 256 dump.raww | sort | uniq -c | sort -n
+
+            cnt_max = int(100e6 / 10000)
+            cnt_max = 99
+
+            # Going faster than this leads to issues:
+            # cnt_max = 4
+
             self.sync += [
                 If (~fifo_tx.writable,
                     overflow.eq(1)
                 ),
-                If (counter == 100,
+
+                If (counter == cnt_max,
                     counter.eq(0),
                     counter2.eq(counter2 + 2)
                 ).Else(
--- software/kilsyth/gateware/ft600/__init__.py
+++ software/kilsyth/gateware/ft600/__init__.py
@@ -4,9 +4,7 @@ __all__ = ["FT600"]
 
 class FT600(Module):
     """
-    FT600 fifo 
-
-    Use ``CEInserter`` and ``ResetInserter`` transformers to control the LFSR.
+    FT600 fifo
 
     ``sys`` clock domain is used for the state machine. Map this to the ft600 clock.
 
@@ -14,10 +12,13 @@ class FT600(Module):
         ft600 IOs
     :param fifo_rx:
         RX fifo, i.e. reading data from the ft600 interface (from the USB host)
-    :type AsyncFifo:
+    :type _FIFOInterface:
     :param fifo_tx:
         TX fifo, i.e. writing data to the ft600 interface (to the USB host)
-    :type AsyncFifo:
+    :type _FIFOInterface:
+    :param debug:
+        State machine debug information
+    :type Signal[3]:
     """
     def __init__(self, ft600, fifo_rx, fifo_tx, debug):
         ft_be_triple = TSTriple(2)
@@ -31,30 +32,55 @@ class FT600(Module):
         self.submodules.fsm = FSM(reset_state="INIT")
         self.fsm.act(
             "INIT",
+            NextValue(debug[0], 1),
+            NextValue(debug[1], 0),
+            NextValue(debug[2], 0),
+
             NextValue(ft600.oe_n, 1),
             NextValue(ft600.rd_n, 1),
             NextValue(ft600.wr_n, 1),
             NextValue(self.ft_be.oe, 0),
             NextValue(self.ft_data.oe, 0),
-            NextValue(debug[0], 1),
-            NextValue(debug[1], 0),
-            NextValue(debug[2], 0),
+
+            NextValue(fifo_tx.re, 0),
+
             NextState("WAIT")
         )
+
+        cache = Signal(16)
+        cache_valid = Signal()
+
+
 
         self.fsm.act(
             "WAIT",
             NextValue(debug[0], 0),
             NextValue(debug[1], 1),
             NextValue(debug[2], 0),
-            NextValue(ft600.wr_n, 1),
 
+            # Idle states for the control pins
+            NextValue(ft600.oe_n, 1),
+            NextValue(ft600.rd_n, 1),
+            NextValue(ft600.wr_n, 1),
             NextValue(self.ft_data.oe, 0),
             NextValue(self.ft_be.oe, 0),
-            NextValue(self.ft_be.o, 0b00),
 
             If ((fifo_tx.readable) & (~ft600.txe_n),
-                NextValue(fifo_tx.re, 1),
+
+                # The FT60x works like this:
+                # 1. When ft600.txe_n is 0, there is space available in the TX buffer.
+                # 2. The clock cycle after ft600.wr_n is asserted,
+                #    BE and DATA will be read until txe_n is released.
+                #    Data will only be read up to the clock cycle just before.
+                # This means that we need to assert ft600.wr_n only when we have data ready.
+
+                # The cache is explained below
+                If (cache_valid,
+                    NextValue(fifo_tx.re, 0),
+                ).Else(
+                    NextValue(fifo_tx.re, 1),
+                ),
+
                 NextState("WRITE")
             )
         )
@@ -64,19 +90,48 @@ class FT600(Module):
             NextValue(debug[0], 0),
             NextValue(debug[1], 0),
             NextValue(debug[2], 1),
+
+            # Tell FT600 that data should be read in the next cycle
             NextValue(ft600.wr_n, 0),
 
-            NextValue(self.ft_data.o, fifo_tx.dout),
-            NextValue(self.ft_data.oe, 1),
-
+            # Tell FT600 that banks 1 and 2 are used
             NextValue(self.ft_be.oe, 1),
             NextValue(self.ft_be.o, 0b11),
+
+            # Tell our fifo that we want to a value in the next cycle
+            NextValue(fifo_tx.re, 1),
+
+            # Used the cached value from the previous transfer if available,
+            # use the value from the FIFO otherwise.
+            If (cache_valid,
+                NextValue(self.ft_data.o, cache),
+                NextValue(cache_valid, 0),
+            ).Else(
+                NextValue(self.ft_data.o, fifo_tx.dout),
+                NextValue(cache, fifo_tx.dout),
+            ),
+
+            # Output enable for the data pins
+            NextValue(self.ft_data.oe, 1),
 
             If ((~fifo_tx.readable) | (ft600.txe_n),
                 # There is nothing to read
                 # or we are not allowed to write to ft600.
                 # Time to wait.
                 NextValue(fifo_tx.re, 0),
+                NextValue(ft600.wr_n, 1),
+                NextValue(self.ft_be.oe, 0),
+                NextValue(self.ft_data.oe, 0),
+
+                # If ft600.txe_n == 1, that means that the ft600
+                # will not read out data that we have just taken
+                # from the FIFO. Cache it.
+                # Before the next transfer, we must use the cache
+                # and not take a value from the FIFO.
+                If (ft600.txe_n,
+                    NextValue(cache_valid, 1),
+                ),
+
                 NextState("WAIT")
             )
         )
